2053e48c152844f2bc7875523bf958ad87d28de2
daniel <daniel@daniels-MacBook-Pro.local>
2026-02-13 00:01:39 -0800

    back to basics

--- source/peg_isaac/peg_isaac/tasks/direct/peg_isaac/__init__.py
+++ source/peg_isaac/peg_isaac/tasks/direct/peg_isaac/__init__.py
@@ -13,14 +13,17 @@ from . import agents
 
 gym.register(
     id="Template-Peg-Isaac-Direct-v0",
-    entry_point=f"{__name__}.peg_isaac_env_marl:PursuitEvasionDirectMARLEnv",
+    # entry_point=f"{__name__}.peg_isaac_env_marl:PursuitEvasionDirectMARLEnv",
     # entry_point=f"{__name__}.peg_isaac_env_multi:QuadcopterEnv",
+    entry_point=f"{__name__}.peg_isaac_env:QuadcopterEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.peg_isaac_env_marl:PursuitEvasionEnvCfg",
-        "skrl_ippo_cfg_entry_point": f"{agents.__name__}:skrl_ippo_jax_cfg.yaml",
+        # "env_cfg_entry_point": f"{__name__}.peg_isaac_env_marl:PursuitEvasionEnvCfg",
+        # "skrl_ippo_cfg_entry_point": f"{agents.__name__}:skrl_ippo_jax_cfg.yaml",
         # "env_cfg_entry_point": f"{__name__}.peg_isaac_env_multi:QuadcopterEnvCfg",
         # "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "env_cfg_entry_point": f"{__name__}.peg_isaac_env:QuadcopterEnvCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
 )
 
